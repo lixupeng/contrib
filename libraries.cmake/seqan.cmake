9eccddc7fc9ad76f54099c4f5e875c7d37912ffa
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-02-14 16:03:09 +0100

    [FEATURE] upgrade of seqan to official 1.4.1 release

--- libraries.cmake/seqan.cmake
+++ libraries.cmake/seqan.cmake
@@ -1,8 +1,8 @@
 ##################################################
-###       SEQAN   														 ###
+###       SEQAN                                ###
 ##################################################
-## 
-## 
+##
+##
 
 MACRO( OPENMS_CONTRIB_BUILD_SEQAN )
   OPENMS_LOGHEADER_LIBRARY("SeqAn")
@@ -14,9 +14,72 @@ MACRO( OPENMS_CONTRIB_BUILD_SEQAN )
   endif()
   OPENMS_SMARTEXTRACT(ZIP_ARGS ARCHIVE_SEQAN "SEQAN" "include/seqan/version.h")
 
-  ## copy includes
-  set(dir_target ${CONTRIB_BIN_INCLUDE_DIR}/seqan)
-  set(dir_source ${SEQAN_DIR}/include/seqan)
-  OPENMS_COPYDIR(dir_source dir_target)
+  # create seqan build system and install to target directory
+  set(_SEQAN_BUILD_DIR "${SEQAN_DIR}/build")
+  file(TO_NATIVE_PATH "${_SEQAN_BUILD_DIR}" _SEQAN_NATIVE_BUILD_DIR)
+
+  execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${_SEQAN_NATIVE_BUILD_DIR})
+
+  message(STATUS "Generating seqan build system .. ")
+  execute_process(COMMAND ${CMAKE_COMMAND}
+                        -G "${CMAKE_GENERATOR}"
+                        -D CMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}
+                        -DSEQAN_BUILD_SYSTEM=SEQAN_RELEASE_LIBRARY
+                        ${SEQAN_DIR}
+                  WORKING_DIRECTORY ${_SEQAN_NATIVE_BUILD_DIR}
+                  OUTPUT_VARIABLE _SEQAN_CMAKE_OUT
+                  ERROR_VARIABLE _SEQAN_CMAKE_ERR
+                  RESULT_VARIABLE _SEQAN_CMAKE_SUCCESS)
+
+  # output to logfile
+  file(APPEND ${LOGFILE} ${_SEQAN_CMAKE_OUT})
+  file(APPEND ${LOGFILE} ${_SEQAN_CMAKE_ERR})
+
+  if (NOT _SEQAN_CMAKE_SUCCESS EQUAL 0)
+    message(FATAL_ERROR "Generating seqan build system .. failed")
+  else()
+    message(STATUS "Generating seqan build system .. done")
+  endif()
+
+  # the install target on windows has a different name then on mac/lnx
+  if(MSVC)
+      set(_SEQAN_INSTALL_TARGET "INSTALL")
+  else()
+      set(_SEQAN_INSTALL_TARGET "install")
+  endif()
+
+  message(STATUS "Building seqan doc  .. ")
+  execute_process(COMMAND ${CMAKE_COMMAND} --build ${_SEQAN_NATIVE_BUILD_DIR} --target docs --config Release
+                  WORKING_DIRECTORY ${_SEQAN_NATIVE_BUILD_DIR}
+                  OUTPUT_VARIABLE _SEQAN_BUILD_DOC_OUT
+                  ERROR_VARIABLE _SEQAN_BUILD_DOC_ERR
+                  RESULT_VARIABLE _SEQAN_BUILD_DOC_SUCCESS)
+
+  if (NOT _SEQAN_BUILD_DOC_SUCCESS EQUAL 0)
+    message(FATAL_ERROR "Building seqan doc .. failed")
+  else()
+    message(STATUS "Building seqan doc .. done")
+  endif()
+
+  # output to logfile
+  file(APPEND ${LOGFILE} ${_SEQAN_BUILD_DOC_OUT})
+  file(APPEND ${LOGFILE} ${_SEQAN_BUILD_DOC_ERR})
+
+  message(STATUS "Installing seqan headers .. ")
+  execute_process(COMMAND ${CMAKE_COMMAND} --build ${_SEQAN_NATIVE_BUILD_DIR} --target ${_SEQAN_INSTALL_TARGET} --config Release
+                  WORKING_DIRECTORY ${_SEQAN_NATIVE_BUILD_DIR}
+                  OUTPUT_VARIABLE _SEQAN_BUILD_OUT
+                  ERROR_VARIABLE _SEQAN_BUILD_ERR
+                  RESULT_VARIABLE _SEQAN_BUILD_SUCCESS)
+
+  # output to logfile
+  file(APPEND ${LOGFILE} ${_SEQAN_BUILD_OUT})
+  file(APPEND ${LOGFILE} ${_SEQAN_BUILD_ERR})
+
+  if (NOT _SEQAN_BUILD_SUCCESS EQUAL 0)
+    message(FATAL_ERROR "Installing seqan headers .. failed")
+  else()
+    message(STATUS "Installing seqan headers .. done")
+  endif()
 
 ENDMACRO( OPENMS_CONTRIB_BUILD_SEQAN )
