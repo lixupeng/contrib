f347746c39fe4f2a956b5c04da544cf16c6d3565
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2015-12-11 11:43:40 +0100

    Update coinor.cmake
    List new patch in cmake script of coinor.

--- libraries.cmake/coinor.cmake
+++ libraries.cmake/coinor.cmake
@@ -23,6 +23,10 @@ MACRO( OPENMS_CONTRIB_BUILD_COINOR)
     set(PATCH_FILE "${PATCH_DIR}/coinor/CoinLpIO.diff")
     set(PATCHED_FILE "${COINOR_DIR}/CoinUtils/src/CoinLpIO.cpp")
     OPENMS_PATCH( PATCH_FILE COINOR_DIR PATCHED_FILE)
+    
+    set(PATCH_FILE "${PATCH_DIR}/coinor/CoinTypes.hpp.diff")
+    set(PATCHED_FILE "${COINOR_DIR}/CoinUtils/src/CoinTypes.hpp")
+    OPENMS_PATCH( PATCH_FILE COINOR_DIR PATCHED_FILE)
 	
     set(MSBUILD_ARGS_SLN "${COINOR_DIR}/CoinMP/MSVisualStudio/v${CONTRIB_MSVC_VERSION}/CoinMP.sln")
     set(MSBUILD_ARGS_TARGET "libCbc")
