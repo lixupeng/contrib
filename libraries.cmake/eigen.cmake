6ceb82248a8ecefe8a4eb3ef9371f9fe8cef51be
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-12-01 12:05:25 +0100

    Eigen workaround. Directly call the installation routine.
    We do not want to test Eigen anyway.

--- libraries.cmake/eigen.cmake
+++ libraries.cmake/eigen.cmake
@@ -1,20 +1,19 @@
 ##################################################
-###       Eigen3   														 ###
+###                  Eigen3                    ###
 ##################################################
 
 ## build and install eigen
 macro( OPENMS_CONTRIB_BUILD_EIGEN )
 
   OPENMS_LOGHEADER_LIBRARY("eigen")
-
-	if(MSVC)
+  if(MSVC)
     set(ZIP_ARGS "x -y -osrc")
   else()
     set(ZIP_ARGS "xzf")
   endif()
   OPENMS_SMARTEXTRACT(ZIP_ARGS ARCHIVE_EIGEN "EIGEN" "CMakeLists.txt")
 
-	if(MSVC)
+  if(MSVC)
     # fixes MSVC 2012 compiler detection
     set(_PATCH_FILE "${PATCH_DIR}/eigen/EigenDetermineVSServicePack.cmake.patch")
     set(_PATCHED_FILE "${EIGEN_DIR}/cmake/EigenDetermineVSServicePack.cmake")
@@ -27,36 +26,28 @@ macro( OPENMS_CONTRIB_BUILD_EIGEN )
 
   execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${_EIGEN_NATIVE_BUILD_DIR})
 
-	message(STATUS "Generating eigen build system .. ")
+  message(STATUS "Generating eigen build system .. ")
   execute_process(COMMAND ${CMAKE_COMMAND}
-												-G "${CMAKE_GENERATOR}"
-												-D CMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}
-												${EIGEN_DIR}
-									WORKING_DIRECTORY ${_EIGEN_NATIVE_BUILD_DIR}
-									OUTPUT_VARIABLE _EIGEN_CMAKE_OUT
-									ERROR_VARIABLE _EIGEN_CMAKE_ERR
-									RESULT_VARIABLE _EIGEN_CMAKE_SUCCESS)
+									-G "${CMAKE_GENERATOR}"
+									-D CMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}
+									${EIGEN_DIR}
+                  WORKING_DIRECTORY ${_EIGEN_NATIVE_BUILD_DIR}
+                  OUTPUT_VARIABLE _EIGEN_CMAKE_OUT
+                  ERROR_VARIABLE _EIGEN_CMAKE_ERR
+                  RESULT_VARIABLE _EIGEN_CMAKE_SUCCESS)
 
   # output to logfile
   file(APPEND ${LOGFILE} ${_EIGEN_CMAKE_OUT})
   file(APPEND ${LOGFILE} ${_EIGEN_CMAKE_ERR})
 
-	if (NOT _EIGEN_CMAKE_SUCCESS EQUAL 0)
-		message(FATAL_ERROR "Generating eigen build system .. failed")
-	else()
-		message(STATUS "Generating eigen build system .. done")
-	endif()
-
-  # the install target on windows has a different name then on mac/lnx
-  if(MSVC)
-      set(_EIGEN_INSTALL_TARGET "INSTALL")
+  if (NOT _EIGEN_CMAKE_SUCCESS EQUAL 0)
+    message(FATAL_ERROR "Generating eigen build system .. failed")
   else()
-      set(_EIGEN_INSTALL_TARGET "install")
+    message(STATUS "Generating eigen build system .. done")
   endif()
 
-
-	message(STATUS "Installing eigen headers .. ")
-	execute_process(COMMAND ${CMAKE_COMMAND} --build ${_EIGEN_NATIVE_BUILD_DIR} --target ${_EIGEN_INSTALL_TARGET} --config Release
+  message(STATUS "Installing eigen headers .. ")
+  execute_process(COMMAND ${CMAKE_COMMAND} -DBUILD_TYPE=Release cmake_install.cmake
 									WORKING_DIRECTORY ${_EIGEN_NATIVE_BUILD_DIR}
 									OUTPUT_VARIABLE _EIGEN_BUILD_OUT
 									ERROR_VARIABLE _EIGEN_BUILD_ERR
